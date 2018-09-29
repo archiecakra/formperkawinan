e0cb225344922a5ad3b3b2b84b4d430002fff939
archiecakra <42937208+archiecakra@users.noreply.github.com>
2018-09-29 11:44:05 +0700

    Add files via upload
    Commit setup awal

---
+++ form.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+  <head>
+
+    <meta name="description" content="Form Registrasi Perkawinan">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/fa_all.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/form.css">
+
+    <!-- jQuery -->
+    <script src="assets/js/jquery-3.3.1.min.js"></script>
+
+    <!-- Java script -->
+    <script src="assets/js/bootstrap.bundle.js"></script>
+    <script src="assets/js/register.js"></script>
+
+    <title>Form Registrasi Perkawinan</title>
+
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <div class="card">
+        <div class="card-header">
+          <ul class="nav nav-tabs card-header-tabs">
+            <li class="nav item text-center">
+              <a href="#" class="nav-link active">Data Calon<br>Mempelai Pria</a>
+            </li>
+            <li class="nav item text-center">
+              <a href="#" class="nav-link">Data Calon<br>Mempelai Wanita</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+    </div>
+
+  </body>
+
+</html>
